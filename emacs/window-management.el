bf2657f2bc28a6292faba22b04c18efe4ac75ca3
Nam sanghyeok <simian114@gmail.com>
2025-04-19 00:05:14 +0900

    add swap window

--- emacs/window-management.el
+++ emacs/window-management.el
@@ -24,10 +24,40 @@ Window Resize: _j_ shrink height | _k_ enlarge height | _k_ shrink width | _l_ e
     ("l" enlarge-window-horizontally)
     ("q" nil :exit t)))
 
+(defun my-swap-window-buffers-with-direction ()
+  "Swap buffers with a window selected by direction."
+  (interactive)
+  (let* ((current-win (selected-window))
+         (current-buf (window-buffer current-win))
+         (direction
+          (read-char-choice
+           "Swap with window (h: left, l: right, k: up, j: down): "
+           '(?h ?l ?k ?j)))
+         (other-win
+          (pcase direction
+            (?h (windmove-left))
+            (?l (windmove-right))
+            (?k (windmove-up))
+            (?j (windmove-down)))))
+    (if (and other-win (not (eq other-win current-win)))
+        (let ((other-buf (window-buffer other-win)))
+          (set-window-buffer current-win other-buf)
+          (set-window-buffer other-win current-buf)
+          (select-window current-win)
+          (message "Swapped buffers with %s window"
+                   (pcase direction
+                     (?h "left")
+                     (?l "right")
+                     (?k "up")
+                     (?j "down"))))
+      (message "No window in that direction")
+      (select-window current-win))))
+
 ;; Update Meow Leader keymap to include hydra
 (meow-leader-define-key
  '("w L" . enlarge-window-horizontally)  ;; Keep for single press
  '("w H" . shrink-window-horizontally)
  '("w K" . enlarge-window)
  '("w J" . shrink-window)
- '("w r" . hydra-window-resize/body))  ;; SPC w r to start hydra
+ '("w r" . hydra-window-resize/body)  ;; SPC w r to start hydra
+ '("w s" . my-swap-window-buffers-with-direction)) ;; Swap with selected window
